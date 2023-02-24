1d9942d6306f8a1065b6fd29f6c77c6ec8811511
Daniel Choi <dchoi87@gmail.com>
2023-02-23 19:47:17 -0800

    Initial Commit

--- README.md
+++ README.md
@@ -1,2 +1,29 @@
-# cedars-aem-extension
-Cedars AEM Chrome Extension
+# Cedars AEM Chrome Extension
+
+## Instructions
+
+1. Download zip file and unpack
+2. Go to chrome extensions menu (`chrome://extensions`)
+3. Toggle **Developer Mode** and click **Load Unpacked**
+4. Select unpacked folder
+
+![Extensions Menu](./extensions.png)
+
+5. (Optional) Pin to your address bar if it doesn't automatically show up
+
+![Pin](./pin.png)
+
+## Usage
+
+From any AEM page (editor, dispatcher, etc.), click on the appropriate button to navigate to that url.
+
+### Examples
+
+- Editor URL to Dispatcher URL
+- Dispatcher URL to Editor Published URL (`wcmmode=disabled`)
+- Editor Published URL to AEM URL (sites.html)
+
+## Notes
+
+- Works only for Dev and Stage
+- Works only for pages under `cedars-sinai`. Using this on a root level page (far left column in AEM) will probably error out
---
+++ index.js
@@ -0,0 +1,73 @@
+const extension = {
+  $btns: document.querySelectorAll("button"),
+  init: function () {
+    const _self = this;
+
+    if (_self.$btns.length) {
+      _self.$btns.forEach((btn) => {
+        const data_url = btn.dataset.url;
+        const data_id = btn.dataset.id;
+
+        btn.addEventListener("click", () => {
+          if (chrome.tabs) {
+            const config = { active: true, currentWindow: true };
+
+            chrome.tabs.query(config, (tabs) => {
+              const tab = tabs[0];
+              const url = new URL(tab.url);
+              const target = _self.getTargetUrl(url, data_url, data_id);
+
+              chrome.tabs.update(tab.id, { url: target });
+              window.close();
+            });
+          }
+        });
+      });
+    }
+  },
+  getTargetUrl: function (url, data_url, data_id) {
+    const path = this.getPath(url);
+    const env = this.getEnv(url);
+
+    switch (data_id) {
+      case "aem": {
+        return data_url.replace("$env", env).replace("$path", path.noExt);
+      }
+      case "published": {
+        return (
+          data_url.replace("$env", env).replace("$path", path.withExt) +
+          "?wcmmode=disabled"
+        );
+      }
+      default: {
+        return data_url.replace("$env", env).replace("$path", path.withExt);
+      }
+    }
+  },
+  getPath: function (url) {
+    const path = url.pathname;
+    const arr = path.split("/");
+    const index = arr.indexOf("cedars-sinai");
+    const split_path = index > -1 ? arr.slice(index + 1) : arr.slice(1);
+    const constructed_path = split_path.join("/");
+    const withExtension = constructed_path.includes(".html")
+      ? constructed_path
+      : `${constructed_path}.html`;
+    const noExtension = constructed_path.replace(".html", "");
+
+    return {
+      array: split_path,
+      withExt: withExtension,
+      noExt: noExtension,
+    };
+  },
+  getEnv: function (url) {
+    return url.origin.includes("-dev")
+      ? "dev"
+      : url.origin.includes("-stage")
+      ? "stage"
+      : "prod";
+  },
+};
+
+extension.init();
